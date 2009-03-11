baca538ea22f46dad3ccd2824c85355849dbd6fd
andrei.shindyapin <andrei.shindyapin@gmail.com>
2009-03-11 08:40:24 -0700

    dunno

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,23 +1,22 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
+  
   helper :all # include all helpers, all the time
+  
   uses_tiny_mce(:options => {:theme => 'advanced',
-                           :editor_selector => "mceEditor",
-                           :browsers => %w{msie gecko},
-                           :theme_advanced_toolbar_location => "top",
-                           :theme_advanced_toolbar_align => "left",
-                           :theme_advanced_resizing => true,
-                           :theme_advanced_resize_horizontal => false,
-                           :paste_auto_cleanup_on_paste => true,
-                           :theme_advanced_buttons1 => %w{formatselect fontselect fontsizeselect bold italic underline strikethrough
-                              separator justifyleft justifycenter justifyright indent outdent separator bullist numlist forecolor
-                              backcolor separator link unlink image undo redo},
-                           :theme_advanced_buttons2 => [],
-                           :theme_advanced_buttons3 => [],
-                           :plugins => %w{contextmenu paste}},
-              :only => [:new, :edit, :show, :index])
+                             :editor_selector => "mceEditor",
+                             :browsers => %w{msie gecko},
+                             :theme_advanced_toolbar_location => "top",
+                             :theme_advanced_toolbar_align => "left",
+                             :theme_advanced_resizing => true,
+                             :theme_advanced_resize_horizontal => true,
+                             :paste_auto_cleanup_on_paste => false,
+                             :theme_advanced_buttons1 => %w{formatselect fontselect fontsizeselect bold italic underline strikethrough
+                               separator justifyleft justifycenter justifyright indent outdent separator bullist numlist forecolor
+                               backcolor separator link unlink image undo redo separator search replace separator code},
+                             :theme_advanced_buttons2 => [],
+                             :theme_advanced_buttons3 => [],
+                             :plugins => %w{contextmenu paste searchreplace}},
+                               :only => [:new, :edit, :show, :index])
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
